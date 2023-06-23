3cc1f746815b39423b080045aca271fddc027929
Aelith <unelithally333@gmail.com>
2023-06-23 10:54:35 +0200

    Add mod skeleton with Origins as dependency

---
+++ src/main/java/aelita/dark_origins/DarkOriginsMod.java
@@ -0,0 +1,20 @@
+package aelita.dark_origins;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DarkOriginsMod.MOD_ID)
+public class DarkOriginsMod {
+	public static final String MOD_ID = "aelitas_dark_origins";
+	public static final Logger LOGGER = LogManager.getLogger(DarkOriginsMod.MOD_ID);
+
+	public DarkOriginsMod() {
+		initRegistries();
+	}
+
+	private void initRegistries() {
+
+	}
+}
