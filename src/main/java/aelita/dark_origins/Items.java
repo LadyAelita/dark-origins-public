8c543b303904042f06f5909d86e8225f46b4e016
Aelith <unelithally333@gmail.com>
2023-06-24 11:54:08 +0200

    Add skeleton for Vampire origin

--- src/main/java/aelita/dark_origins/Items.java
+++ src/main/java/aelita/dark_origins/Items.java
@@ -7,6 +7,7 @@ import aelita.dark_origins.item.RitualDaggerItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,6 +23,11 @@ public class Items {
 	public static final RegistryObject<Item> BROOM = REGISTRY.register(Broom.ID, Broom.factory);
 	public static final RegistryObject<Item> RITUAL_DAGGER = REGISTRY.register(RitualDaggerItem.ID, RitualDaggerItem.factory);
 
+	// "Icon items" - that is, unobtainable items that have no function outside
+	//  of having a specific texture. This can be used, for example, as icons
+	//  for custom origins
+	public static final RegistryObject<Item> VAMPIRE_LIPS_ICON = registerIconItem("vampire_lips_icon");
+
 	private static RegistryObject<Item> registerBlockItem(RegistryObject<Block> block) {
 		final var blockName = block.getId().getPath();
 		return REGISTRY.register(blockName, () -> new BlockItem(block.get(), new Item.Properties()));
@@ -29,5 +35,11 @@ public class Items {
 	private static RegistryObject<Item> registerBlockItem(RegistryObject<Block> block, CreativeModeTab tab) {
 		final var blockName = block.getId().getPath();
 		return REGISTRY.register(blockName, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+	}
+
+	private static RegistryObject<Item> registerIconItem(String id) {
+		return REGISTRY.register(id, () -> new Item(
+			(new Item.Properties()).rarity(Rarity.EPIC)
+		));
 	}
 }
