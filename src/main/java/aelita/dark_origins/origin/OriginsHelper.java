38cc88659fb9f50998fa5bd4f741638959abe12b
Aelith <unelithally333@gmail.com>
2023-06-24 20:40:54 +0200

    Streamline OriginsHelper, removing calls to deprecated methods

--- src/main/java/aelita/dark_origins/origin/OriginsHelper.java
+++ src/main/java/aelita/dark_origins/origin/OriginsHelper.java
@@ -6,49 +6,35 @@ import javax.annotation.Nullable;
 
 import aelita.dark_origins.util.ResourceLocationUtils;
 import io.github.apace100.origins.Origins;
-import io.github.edwinmindcraft.origins.api.OriginsAPI;
 import io.github.edwinmindcraft.origins.api.capabilities.IOriginContainer;
 import io.github.edwinmindcraft.origins.api.origin.Origin;
 import io.github.edwinmindcraft.origins.api.origin.OriginLayer;
-import net.minecraft.core.Registry;
+import io.github.edwinmindcraft.origins.api.registry.OriginsDynamicRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.util.LazyOptional;
 
 public class OriginsHelper {
 	public static final ResourceLocation DEFAULT_LAYER_ID = new ResourceLocation(Origins.MODID, "origin");
+	public static final ResourceKey<OriginLayer> DEFAULT_LAYER_KEY = getOriginLayerKey(DEFAULT_LAYER_ID);
 
-	public static @Nullable OriginLayer getOriginsLayer(Registry<OriginLayer> layerRegistry, ResourceLocation originLayerId) {
-		return layerRegistry.get(originLayerId);
-	}
-	public static @Nullable OriginLayer getOriginsLayer(MinecraftServer server, ResourceLocation originLayerId) {
-		return getOriginsLayer(OriginsAPI.getLayersRegistry(server), originLayerId);
-	}
-	public static @Nullable OriginLayer getOriginsLayer(ResourceLocation originLayerId) {
-		return getOriginsLayer(OriginsAPI.getLayersRegistry(), originLayerId);
-	}
-	public static @Nullable OriginLayer getOriginsLayer(Player player, ResourceLocation originLayerId) {
-		MinecraftServer server = player.getServer();
-		if (server != null) {
-			return getOriginsLayer(server, originLayerId);
-		} else {
-			return getOriginsLayer(originLayerId);
-		}
+	public static ResourceKey<OriginLayer> getOriginLayerKey(ResourceLocation originLayerId) {
+		return ResourceKey.create(OriginsDynamicRegistries.LAYERS_REGISTRY, originLayerId);
 	}
 
-	public static @Nullable ResourceKey<Origin> getPlayerOriginKey(Player player, ResourceLocation originLayerId) {
-		OriginLayer layer = getOriginsLayer(player, originLayerId);
-		if (layer == null) {
-			// TODO: Log warning here?
-			return null;
-		}
+	public static ResourceKey<Origin> getOriginKey(ResourceLocation originId) {
+		return ResourceKey.create(OriginsDynamicRegistries.ORIGINS_REGISTRY, originId);
+	}
+
+	public static ResourceKey<Origin> getPlayerOriginKey(Player player, ResourceLocation originLayerId) {
 		IOriginContainer origins = getPlayerOriginContainer(player);
 		if (origins == null) {
 			// TODO: Log warning here?
 			return null;
 		}
-		return origins.getOrigin(layer);
+		ResourceKey<OriginLayer> layerKey = getOriginLayerKey(originLayerId);
+		return origins.getOrigin(layerKey);
 	}
 	public static @Nullable ResourceKey<Origin> getPlayerOriginKey(Player player) {
 		return getPlayerOriginKey(player, DEFAULT_LAYER_ID);
@@ -77,10 +63,12 @@ public class OriginsHelper {
 	}
 
 	public static @Nullable IOriginContainer getPlayerOriginContainer(Player player) {
-		Optional<IOriginContainer> containerOptional = player.getCapability(OriginsAPI.ORIGIN_CONTAINER).resolve();
+		LazyOptional<IOriginContainer> containerLazyOptional = IOriginContainer.get(player);
+		Optional<IOriginContainer> containerOptional = containerLazyOptional.resolve();
 		if (containerOptional.isEmpty()) {
 			return null;
 		}
-		return containerOptional.get();
+		IOriginContainer container = containerOptional.get();
+		return container;
 	}
 }
