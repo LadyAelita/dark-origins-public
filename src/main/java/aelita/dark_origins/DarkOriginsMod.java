c984c9973a77b550ee9d5765ecb530958b9a1924
Aelita <unelithally333@gmail.com>
2023-09-06 01:35:02 +0200

    Reproduce the bug using only vanilla items
    For EdwinMindcraft/origins-architectury#353

--- src/main/java/aelita/dark_origins/DarkOriginsMod.java
+++ src/main/java/aelita/dark_origins/DarkOriginsMod.java
@@ -3,22 +3,12 @@ package aelita.dark_origins;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(DarkOriginsMod.MOD_ID)
 public class DarkOriginsMod {
 	public static final String MOD_ID = "aelitas_dark_origins";
 	public static final Logger LOGGER = LogManager.getLogger(DarkOriginsMod.MOD_ID);
 
-	public DarkOriginsMod() {
-		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		initRegistries(modEventBus);
-	}
-
-	private void initRegistries(IEventBus bus) {
-		// All registries must be hooked up this way
-		Items.REGISTRY.register(bus);
-	}
+	public DarkOriginsMod() {}
 }
