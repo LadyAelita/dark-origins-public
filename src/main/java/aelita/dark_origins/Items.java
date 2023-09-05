f83c1c9c3e18ada3bc61ba4907d0bd0bdcbb0f1c
Aelita <unelithally333@gmail.com>
2023-09-06 01:09:07 +0200

    Create minimal reproduction of the Sunscreen freeze bug
    For EdwinMindcraft/origins-architectury#353

--- src/main/java/aelita/dark_origins/Items.java
+++ src/main/java/aelita/dark_origins/Items.java
@@ -1,27 +1,7 @@
 package aelita.dark_origins;
 
-import java.util.function.Supplier;
-
-import aelita.dark_origins.item.AnimalBloodItem;
-import aelita.dark_origins.item.BloodItem;
-import aelita.dark_origins.item.Broom;
-import aelita.dark_origins.item.CoagulatedBloodItem;
-import aelita.dark_origins.item.EnchantedBloodItem;
-import aelita.dark_origins.item.EnderBloodItem;
-import aelita.dark_origins.item.HemolymphItem;
-import aelita.dark_origins.item.IllagerBloodItem;
-import aelita.dark_origins.item.PhantomEssenceItem;
-import aelita.dark_origins.item.PoisonousHemolymphItem;
-import aelita.dark_origins.item.RitualDaggerItem;
-import aelita.dark_origins.item.SpiderHemolymphItem;
 import aelita.dark_origins.item.SunscreenBottle;
-import aelita.dark_origins.item.VampireBloodItem;
-import aelita.dark_origins.item.VillagerBloodItem;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -30,77 +10,5 @@ public class Items {
 	public static final DeferredRegister<Item> REGISTRY
 		= DeferredRegister.create(ForgeRegistries.ITEMS, DarkOriginsMod.MOD_ID);
 
-	// Blood
-	public static final RegistryObject<Item> BLOOD = REGISTRY.register(BloodItem.ID, BloodItem.factory);
-	public static final RegistryObject<Item> ANIMAL_BLOOD = REGISTRY.register(AnimalBloodItem.ID, AnimalBloodItem.factory);
-	public static final RegistryObject<Item> COAGULATED_BLOOD = REGISTRY.register(CoagulatedBloodItem.ID, CoagulatedBloodItem.factory);
-	public static final RegistryObject<Item> ENCHANTED_BLOOD = REGISTRY.register(EnchantedBloodItem.ID, EnchantedBloodItem.factory);
-	public static final RegistryObject<Item> ENDER_BLOOD = REGISTRY.register(EnderBloodItem.ID, EnderBloodItem.factory);
-	public static final RegistryObject<Item> ILLAGER_BLOOD = REGISTRY.register(IllagerBloodItem.ID, IllagerBloodItem.factory);
-	public static final RegistryObject<Item> PHANTOM_ESSENCE = REGISTRY.register(PhantomEssenceItem.ID, PhantomEssenceItem.factory);
-	public static final RegistryObject<Item> VAMPIRE_BLOOD = REGISTRY.register(VampireBloodItem.ID, VampireBloodItem.factory);
-	public static final RegistryObject<Item> VILLAGER_BLOOD = REGISTRY.register(VillagerBloodItem.ID, VillagerBloodItem.factory);
-	public static final RegistryObject<Item> HEMOLYMPH = REGISTRY.register(HemolymphItem.ID, HemolymphItem.factory);
-	public static final RegistryObject<Item> SPIDER_HEMOLYMPH = REGISTRY.register(SpiderHemolymphItem.ID, SpiderHemolymphItem.factory);
-	public static final RegistryObject<Item> POISONOUS_HEMOLYMPH = REGISTRY.register(PoisonousHemolymphItem.ID, PoisonousHemolymphItem.factory);
-
-	// Bottled blood
-	public static final RegistryObject<Item> BLOOD_BOTTLE = registerBloodBottleItem(BloodItem.ID, BloodItem.bottleFactory);
-	public static final RegistryObject<Item> ANIMAL_BLOOD_BOTTLE = registerBloodBottleItem(AnimalBloodItem.ID, AnimalBloodItem.bottleFactory);
-	// public static final RegistryObject<Item> COAGULATED_BLOOD_BOTTLE = registerBloodBottleItem(CoagulatedBloodItem.ID, CoagulatedBloodItem.bottleFactory);
-	public static final RegistryObject<Item> ENCHANTED_BLOOD_BOTTLE = registerBloodBottleItem(EnchantedBloodItem.ID, EnchantedBloodItem.bottleFactory);
-	public static final RegistryObject<Item> ENDER_BLOOD_BOTTLE = registerBloodBottleItem(EnderBloodItem.ID, EnderBloodItem.bottleFactory);
-	public static final RegistryObject<Item> ILLAGER_BLOOD_BOTTLE = registerBloodBottleItem(IllagerBloodItem.ID, IllagerBloodItem.bottleFactory);
-	// public static final RegistryObject<Item> PHANTOM_ESSENCE_BOTTLE = registerBloodBottleItem(PhantomEssenceItem.ID, PhantomEssenceItem.bottleFactory);
-	public static final RegistryObject<Item> VAMPIRE_BLOOD_BOTTLE = registerBloodBottleItem(VampireBloodItem.ID, VampireBloodItem.bottleFactory);
-	public static final RegistryObject<Item> VILLAGER_BLOOD_BOTTLE = registerBloodBottleItem(VillagerBloodItem.ID, VillagerBloodItem.bottleFactory);
-	public static final RegistryObject<Item> HEMOLYMPH_BOTTLE = registerBloodBottleItem(HemolymphItem.ID, HemolymphItem.bottleFactory);
-	public static final RegistryObject<Item> SPIDER_HEMOLYMPH_BOTTLE = registerBloodBottleItem(SpiderHemolymphItem.ID, SpiderHemolymphItem.bottleFactory);
-	public static final RegistryObject<Item> POISONOUS_HEMOLYMPH_BOTTLE = registerBloodBottleItem(PoisonousHemolymphItem.ID, PoisonousHemolymphItem.bottleFactory);
-
-	// Blood vials
-	public static final RegistryObject<Item> BLOOD_VIAL = registerBloodBottleVial(BloodItem.ID, BloodItem.vialFactory);
-	public static final RegistryObject<Item> ANIMAL_BLOOD_VIAL = registerBloodBottleVial(AnimalBloodItem.ID, AnimalBloodItem.vialFactory);
-	// public static final RegistryObject<Item> COAGULATED_BLOOD_VIAL = registerBloodBottleVial(CoagulatedBloodItem.ID, CoagulatedBloodItem.vialFactory);
-	public static final RegistryObject<Item> ENCHANTED_BLOOD_VIAL = registerBloodBottleVial(EnchantedBloodItem.ID, EnchantedBloodItem.vialFactory);
-	public static final RegistryObject<Item> ENDER_BLOOD_VIAL = registerBloodBottleVial(EnderBloodItem.ID, EnderBloodItem.vialFactory);
-	public static final RegistryObject<Item> ILLAGER_BLOOD_VIAL = registerBloodBottleVial(IllagerBloodItem.ID, IllagerBloodItem.vialFactory);
-	// public static final RegistryObject<Item> PHANTOM_ESSENCE_VIAL = registerBloodBottleVial(PhantomEssenceItem.ID, PhantomEssenceItem.vialFactory);
-	public static final RegistryObject<Item> VAMPIRE_BLOOD_VIAL = registerBloodBottleVial(VampireBloodItem.ID, VampireBloodItem.vialFactory);
-	public static final RegistryObject<Item> VILLAGER_BLOOD_VIAL = registerBloodBottleVial(VillagerBloodItem.ID, VillagerBloodItem.vialFactory);
-	public static final RegistryObject<Item> HEMOLYMPH_VIAL = registerBloodBottleVial(HemolymphItem.ID, HemolymphItem.vialFactory);
-	public static final RegistryObject<Item> SPIDER_HEMOLYMPH_VIAL = registerBloodBottleVial(SpiderHemolymphItem.ID, SpiderHemolymphItem.vialFactory);
-	public static final RegistryObject<Item> POISONOUS_HEMOLYMPH_VIAL = registerBloodBottleVial(PoisonousHemolymphItem.ID, PoisonousHemolymphItem.vialFactory);
-
-	public static final RegistryObject<Item> BROOM = REGISTRY.register(Broom.ID, Broom.factory);
-	public static final RegistryObject<Item> RITUAL_DAGGER = REGISTRY.register(RitualDaggerItem.ID, RitualDaggerItem.factory);
 	public static final RegistryObject<Item> SUNSCREEN_BOTTLE = REGISTRY.register(SunscreenBottle.ID, SunscreenBottle.factory);
-
-	// "Icon items" - that is, unobtainable items that have no function outside
-	//  of having a specific texture. This can be used, for example, as icons
-	//  for custom origins
-	public static final RegistryObject<Item> VAMPIRE_LIPS_ICON = registerIconItem("vampire_lips_icon");
-
-	private static RegistryObject<Item> registerBlockItem(RegistryObject<Block> block) {
-		final var blockName = block.getId().getPath();
-		return REGISTRY.register(blockName, () -> new BlockItem(block.get(), new Item.Properties()));
-	}
-	private static RegistryObject<Item> registerBlockItem(RegistryObject<Block> block, CreativeModeTab tab) {
-		final var blockName = block.getId().getPath();
-		return REGISTRY.register(blockName, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-	}
-
-	private static RegistryObject<Item> registerIconItem(String id) {
-		return REGISTRY.register(id, () -> new Item(
-			(new Item.Properties()).rarity(Rarity.EPIC)
-		));
-	}
-
-	private static RegistryObject<Item> registerBloodBottleItem(final String itemId, final Supplier<Item> factory) {
-		return REGISTRY.register(itemId + "_bottle", factory);
-	}
-
-	private static RegistryObject<Item> registerBloodBottleVial(final String itemId, final Supplier<Item> factory) {
-		return REGISTRY.register(itemId + "_vial", factory);
-	}
 }
--- src/main/java/aelita/dark_origins/origin/OriginLocations.java
+++
@@ -1,9 +0,0 @@
-package aelita.dark_origins.origin;
-
-import aelita.dark_origins.DarkOriginsMod;
-import net.minecraft.resources.ResourceLocation;
-
-public class OriginLocations {
-	public static final ResourceLocation WITCH = new ResourceLocation(DarkOriginsMod.MOD_ID, "witch");
-	public static final ResourceLocation VAMPIRE = new ResourceLocation(DarkOriginsMod.MOD_ID, "vampire");
-}
